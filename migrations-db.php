0c16373ed25c8be7a8c1383f4a1e5cbeffc7cf1d
Bia Verly <biaverly@bia.verly@oliveiratrust.com.br>
2023-08-10 14:17:43 -0300

    Doctrine migrations config 1

---
+++ migrations-db.php
@@ -0,0 +1,10 @@
+
+<?php
+
+return [
+    'dbname'   => 'Study', // Replace with your actual database name
+    'user'     => 'root',     // Replace with your MySQL username
+    'password' => 'root',     // Replace with your MySQL password
+    'host'     => 'localhost:3301',         // Replace with your MySQL server hostname or IP address
+    'driver'   => 'pdo_mysql',
+];
